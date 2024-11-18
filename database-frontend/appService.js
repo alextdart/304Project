f22f043da484ac57738dcea862752257a0ebd681
Anna Friesen <annafriesen@shaw.ca>
2024-11-18 15:57:13 -0800

    Added CREATE TABLES queries

--- database-frontend/appService.js
+++ database-frontend/appService.js
@@ -78,7 +78,7 @@ async function testOracleConnection() {
 
 async function fetchDemotableFromDb() {
     return await withOracleDB(async (connection) => {
-        const result = await connection.execute('SELECT * FROM DEMOTABLE');
+        const result = await connection.execute('SELECT * FROM User');
         return result.rows;
     }).catch(() => {
         return [];
@@ -88,16 +88,147 @@ async function fetchDemotableFromDb() {
 async function initiateDemotable() {
     return await withOracleDB(async (connection) => {
         try {
-            await connection.execute(`DROP TABLE DEMOTABLE`);
+            await connection.execute(`DROP TABLE Allergy`);
+            await connection.execute(`DROP TABLE UserHasAllergy`);
+            await connection.execute(`DROP TABLE User`);
+            await connection.execute(`DROP TABLE PremiumUser`);
+            await connection.execute(`DROP TABLE StandardUser`);
+            await connection.execute(`DROP TABLE BudgetUser`);
+            await connection.execute(`DROP TABLE UserCreatesMealPlan`);
+            await connection.execute(`DROP TABLE Rating`);
+            await connection.execute(`DROP TABLE Equipment`);
+            await connection.execute(`DROP TABLE EquipmentLocations`);
+            await connection.execute(`DROP TABLE Recipe`);
+            await connection.execute(`DROP TABLE RecipeHasIngredient`);
+            await connection.execute(`DROP TABLE MealPlanContainsRecipe`);
+            await connection.execute(`DROP TABLE Ingredient`);
+            await connection.execute(`DROP TABLE IngredientNutritionalInfo`);
+            await connection.execute(`DROP TABLE MealPlan`);
+            await connection.execute(`DROP TABLE GroceryList`);
+            await connection.execute(`DROP TABLE GroceryListContainsIngredient`);
         } catch(err) {
             console.log('Table might not exist, proceeding to create...');
         }
 
         const result = await connection.execute(`
-            CREATE TABLE DEMOTABLE (
-                id NUMBER PRIMARY KEY,
-                name VARCHAR2(20)
-            )
+            CREATE TABLE Allergy (type VARCHAR PRIMARY KEY),
+
+            CREATE TABLE UserHasAllergy
+            (allergyType VARCHAR,
+            userID INTEGER,
+            severity VARCHAR,
+            PRIMARY KEY (allergyType, userID),
+            FOREIGN KEY allergyType REFERENCES Allergy(allergyType)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
+            FOREIGN KEY userID REFERENCES User(userID)),
+
+            CREATE TABLE User
+            (userID INTEGER PRIMARY KEY,
+            fullName VARCHAR,
+            country VARCHAR,
+            userPrefID INTEGER UNIQUE,
+            cuisine VARCHAR,
+            diet VARCHAR,
+            groceryStore VARCHAR),
+
+            CREATE TABLE PremiumUser
+            (userID INTEGER PRIMARY KEY,
+            nutritionalAdvisorName VARCHAR,
+            FOREIGN KEY userID REFERENCES User(userID)),
+
+            CREATE TABLE StandardUser
+            (userID INTEGER PRIMARY KEY,
+            mealPlanLimit INTEGER,
+            FOREIGN KEY userID REFERENCES User(userID))),
+
+            CREATE TABLE BudgetUser
+            (userID INTEGER PRIMARY KEY,
+            studentDiscount FLOAT,
+            FOREIGN KEY userID REFERENCES User(userID))),
+
+            CREATE TABLE UserCreatesMealPlan
+            (userID INTEGER,
+            mealPlanID INTEGER,
+            PRIMARY KEY (userID, mealPlanID),
+            FOREIGN KEY userID REFERENCES User(userID),
+            FOREIGN KEY mealPlanID REFERENCES MealPlan(mealPlanID)
+            ON DELETE SET NULL
+            ON UPDATE CASCADE),
+
+            CREATE TABLE Rating
+            (ratingID INTEGER PRIMARY KEY,
+            overallRating INTEGER,
+            difficultlyRating INTEGER,
+            userID INTEGER NOT NULL,
+            recipeID INTEGER NOT NULL,
+            FOREIGN KEY userID REFERENCES User(userID),
+            FOREIGN KEY recipeID REFERENCES Recipe(ID)),
+
+            CREATE TABLE Equipment
+            (equipmentID INTEGER,
+            equipmentType VARCHAR,
+            PRIMARY KEY (equipmentID, equipmentType)),
+
+            CREATE TABLE EquipmentLocations
+            (equipmentType VARCHAR PRIMARY KEY,
+            equipmentLocation VARCHAR),
+
+            CREATE TABLE Recipe
+            (ID INTEGER PRIMARY KEY,
+            name VARCHAR,
+            author VARCHAR),
+
+            CREATE TABLE RecipeHasIngredient
+            (recipeID INTEGER,
+            ingredientName VARCHAR,
+            quantity INTEGER,
+            PRIMARY KEY (recipeID, ingredientName),
+            FOREIGN KEY recipeID REFERENCES Recipe(ID),
+            FOREIGN KEY ingredientName REFERENCES Ingredient(name)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE),
+
+            CREATE TABLE MealPlanContainsRecipe
+            (mealPlanID INTEGER,
+            recipeID INTEGER,
+            PRIMARY KEY (mealPlanID, recipeID),
+            FOREIGN KEY mealPlanID REFERENCES MealPlan(mealPlanID),
+            FOREIGN KEY recipeID REFERENCES Recipe(ID)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE),
+
+            CREATE TABLE Ingredient
+            (name VARCHAR PRIMARY KEY,
+            foodGroup VARCHAR),
+
+            CREATE TABLE IngredientNutritionalInfo
+            (name VARCHAR,
+            calories INTEGER,
+            fat INTEGER,
+            protein INTEGER,
+            PRIMARY KEY (name, calories),
+            FOREIGN KEY name REFERENCES Ingredient(name)),
+
+            CREATE TABLE MealPlan
+            (mealPlanID INTEGER PRIMARY KEY,
+            endDate DATE,
+            startDate DATE NOT NULL,
+            groceryListID INTEGER,
+            FOREIGN KEY groceryListID REFERENCES groceryList(groceryListID)),
+
+            CREATE TABLE GroceryList
+            (groceryListID INTEGER PRIMARY KEY,
+            totalPrice INTEGER),
+
+            CREATE TABLE GroceryListContainsIngredient
+            (groceryListID INTEGER,
+            ingredientName VARCHAR,
+            PRIMARY KEY (groceryListID, ingredientName),
+            FOREIGN KEY groceryListID REFERENCES GroceryList(groceryListID),
+            FOREIGN KEY ingredientName REFERENCES Ingredient(name)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE)
         `);
         return true;
     }).catch(() => {
@@ -135,7 +266,7 @@ async function updateNameDemotable(oldName, newName) {
 
 async function countDemotable() {
     return await withOracleDB(async (connection) => {
-        const result = await connection.execute('SELECT Count(*) FROM DEMOTABLE');
+        const result = await connection.execute('SELECT Count(*) FROM User');
         return result.rows[0][0];
     }).catch(() => {
         return -1;
