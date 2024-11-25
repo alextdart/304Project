1f30a0421a193723ff4dedb6f6dd0baef9602f16
alexdart <adart075@gmail.com>
2024-11-25 14:17:34 -0800

    deleted anything with demotable in it

--- Project_Template/appService.js
+++ Project_Template/appService.js
@@ -76,50 +76,6 @@ async function testOracleConnection() {
     });
 }
 
-async function fetchDemotableFromDb() {
-    return await withOracleDB(async (connection) => {
-        const result = await connection.execute('SELECT * FROM DEMOTABLE');
-        return result.rows;
-    }).catch(() => {
-        return [];
-    });
-}
-
-async function initiateDemotable() {
-    return await withOracleDB(async (connection) => {
-        try {
-            await connection.execute(`DROP TABLE DEMOTABLE`);
-        } catch(err) {
-            console.log('Table might not exist, proceeding to create...');
-        }
-
-        const result = await connection.execute(`
-            CREATE TABLE DEMOTABLE (
-                id NUMBER PRIMARY KEY,
-                name VARCHAR2(20)
-            )
-        `);
-        return true;
-    }).catch(() => {
-        return false;
-    });
-}
-
-async function insertDemotable(id, name) {
-    return await withOracleDB(async (connection) => {
-        const result = await connection.execute(
-            `INSERT INTO DEMOTABLE (id, name) VALUES (:id, :name)`,
-            [id, name],
-            { autoCommit: true }
-        );
-
-        return result.rowsAffected && result.rowsAffected > 0;
-    }).catch(() => {
-        return false;
-    });
-}
-
-
 async function insertIngredient(id, name, quantity) {
     return await withOracleDB(async (connection) => {
         const result = await connection.execute(
@@ -189,29 +145,6 @@ async function getClientData() {
             `SELECT * FROM CLIENT`
         );
         return result.rows;
-    });
-}
-
-async function updateNameDemotable(oldName, newName) {
-    return await withOracleDB(async (connection) => {
-        const result = await connection.execute(
-            `UPDATE DEMOTABLE SET name=:newName where name=:oldName`,
-            [newName, oldName],
-            { autoCommit: true }
-        );
-
-        return result.rowsAffected && result.rowsAffected > 0;
-    }).catch(() => {
-        return false;
-    });
-}
-
-async function countDemotable() {
-    return await withOracleDB(async (connection) => {
-        const result = await connection.execute('SELECT Count(*) FROM DEMOTABLE');
-        return result.rows[0][0];
-    }).catch(() => {
-        return -1;
     });
 }
 
@@ -387,7 +320,7 @@ async function updateUserInfo(userID, newFullName, newCountry, newCuisine, newDi
             [userID, newFullName, newCountry, newCuisine, newDiet, newGroceryStore],
             { autoCommit: true }
         );
-        return result.rowsAffected && result.rowsAffected > 0;
+        return result.rows;
     }).catch(() => {
         return false;
     });
@@ -431,19 +364,14 @@ async function getUsersWithMinMealPlans(minMealPlans) {
 
 module.exports = {
     testOracleConnection,
-    fetchDemotableFromDb,
-    initiateDemotable, 
-    insertDemotable,
     insertIngredient,
     selectOverallRating,
     getIngredientData,
     getClientData,
     getRecipeData,
     getRecipeHasIngredientData,
-    updateNameDemotable,
     totalCaloriesPerRecipe,
     findAllergicPeople,
-    countDemotable,
     deleteMealPlan,
     getRecipesWithCaloriesOver,
     getMealPlansCreatedBy,
